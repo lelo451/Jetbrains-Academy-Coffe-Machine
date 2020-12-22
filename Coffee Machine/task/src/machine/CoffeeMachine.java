e0e15c4b48bfe95f0be192b9211cf130995a6a96
lelo451 <ra98995@uem.br>
2020-12-22 15:25:31 -0300

    Stage 2/6: Machines have needs

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,13 +1,18 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Starting to make a coffee\n" +
-                "Grinding coffee beans\n" +
-                "Boiling water\n" +
-                "Mixing boiled water with crushed coffee beans\n" +
-                "Pouring coffee into the cup\n" +
-                "Pouring some milk into the cup\n" +
-                "Coffee is ready!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write how many cups of coffee you will need:");
+        int cups = scanner.nextInt();
+        int water = 200;
+        int milk = 50;
+        int coffeeBeans = 15;
+        System.out.println("For " + cups + " cups of coffee you will need:");
+        System.out.println(water * cups + " ml of water");
+        System.out.println(milk * cups + " ml of milk");
+        System.out.println(coffeeBeans * cups + " g of coffee beans");
     }
 }
