153c875c52b7a2241b83f48df9d91c0196cfbd40
lelo451 <ra98995@uem.br>
2020-12-22 18:47:40 -0300

    Stage 3/6: Enough coffee for everyone

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -4,15 +4,29 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Write how many cups of coffee you will need:");
-        int cups = scanner.nextInt();
         int water = 200;
         int milk = 50;
         int coffeeBeans = 15;
-        System.out.println("For " + cups + " cups of coffee you will need:");
-        System.out.println(water * cups + " ml of water");
-        System.out.println(milk * cups + " ml of milk");
-        System.out.println(coffeeBeans * cups + " g of coffee beans");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Write how many ml of water the coffee machine has: ");
+        int waterAmount = scanner.nextInt();
+        System.out.print("Write how many ml of milk the coffee machine has: ");
+        int milkAmount = scanner.nextInt();
+        System.out.print("Write how many grams of coffee beans the coffee machine has: ");
+        int coffeeBeansAmount = scanner.nextInt();
+        System.out.print("Write how many cups of coffee you will need: ");
+        int cups = scanner.nextInt();
+        int waterTotal = waterAmount / water;
+        int milkTotal = milkAmount / milk;
+        int coffeBeansTotal = coffeeBeansAmount / coffeeBeans;
+        int min = Math.min(Math.min(waterTotal, milkTotal), coffeBeansTotal);
+        if (min < cups) {
+            System.out.println("No, I can make only " + min + " cup(s) of coffee");
+        } else if (min == cups) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (min > cups) {
+            System.out.println("Yes, I can make that amount of coffee (and even " + (min - cups) + " more than that)");
+        }
+
     }
 }
